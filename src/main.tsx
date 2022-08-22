2cad7cbb609bdf1f1f2e7a262927ba3f9863932f
Junior Alves <jrnalves@gmail.com>
2022-08-22 17:46:37 -0300

    install axios and miragejs

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { createServer } from 'miragejs'
 import { ResetCSS } from './styles/reset'
 import { GlobalStyle } from './styles/global'
+
+
+createServer({
+  routes() {
+    this.namespace = "api"
+
+    this.get("/transations", () => {
+      return (
+        {
+          id: 1,
+          title: 'Transação 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      )
+    })
+  }
+})
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
